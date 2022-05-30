bb9d9a2cf0068d40d6160217b7780438bae2aebc
Travis Lange <travislange12@gmail.com>
2022-05-30 09:47:12 -0400

    Added palettegen, paletteuse, and split

--- build-ffmpeg-copy-out.sh
+++ build-ffmpeg-copy-out.sh
@@ -3,3 +3,5 @@ podman build -t ffmpeg-linux-copy-out -f cuda-ubuntu-free.dockerfile
 podman run -itd --name ffmpeg-linux-copy-out ffmpeg-linux-copy-out
 podman cp ffmpeg-linux-copy-out:/app/workspace/bin/ffmpeg bin/ffmpeg
 podman cp ffmpeg-linux-copy-out:/app/workspace/bin/ffprobe bin/ffprobe
+podman stop ffmpeg-linux-copy-out
+podman container rm ffmpeg-linux-copy-out
